34a149aff86b735c430ba7c5fc17ce2405e37014
Chuck Obaze <chuckobaze@gmail.com>
2018-10-31 20:05:41 -0400

    Stopped Here

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
 $(document).foundation()
+
+$(window).scroll(function () {
+
+    if ($(window).scrollTop() > 1) {
+        $('.main-header').css({
+            'background-color': 'rgba(255, 255, 255, 0.5)'});
+    } else {
+        $('.main-header').css({
+            'background-color': 'transparent'});
+    }
+});
+
+
