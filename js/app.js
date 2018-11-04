9b69195313f33a81b6877ec95d4b2fa9f7021f26
Chuck Obaze <chuckobaze@gmail.com>
2018-11-04 17:56:38 -0500

    Stooped here. Fix second section when you pick back up

--- js/app.js
+++ js/app.js
@@ -2,13 +2,20 @@ $(document).foundation()
 
 $(window).scroll(function () {
 
+
+    // Header bg color appears
     if ($(window).scrollTop() > 1) {
         $('.main-header').css({
-            'background-color': 'rgba(255, 255, 255, 0.5)'});
+            'background-color': 'rgba(255, 255, 255, 0.5)'
+        });
     } else {
         $('.main-header').css({
-            'background-color': 'transparent'});
+            'background-color': 'transparent'
+        });
     }
+
+
+    // Footer Date updates
+    var currentYear = (new Date).getFullYear();
+    $("#thisYear").text((new Date).getFullYear());
 });
-
-
