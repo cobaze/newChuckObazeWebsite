e62c801d2121c33d9b547f55c15b8c3646531e00
Chuck Obaze <chuckobaze@gmail.com>
2018-11-05 22:08:42 -0500

    Stopped here. Added Js on dwn btn

--- js/app.js
+++ js/app.js
@@ -8,12 +8,19 @@ $(window).scroll(function () {
             'background-color': 'rgba(255, 255, 255)',
             'border-bottom':'solid rgb(241, 90, 43) 0.3rem'
         });
+
     } else {
         $('.main-header').css({
             'background-color': 'transparent',
             'border-bottom': 'none'
         });
     }
+
+    $('.dwn-btn').click(function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $('#tester').offset().top}, 2000);
+        });
 
     // Footer Date update
     var currentYear = (new Date).getFullYear();
