c1b279fad99f63314183f0311838e6f93a2df944
Chuck Obaze <chuckobaze@gmail.com>
2018-11-28 03:02:12 -0500

    Added jQuery Hover

--- js/app.js
+++ js/app.js
@@ -21,9 +21,9 @@ $(document).ready(function () {
                 'border-bottom': 'solid rgb(241, 90, 43) 0.3rem'
             });
 
-//            $('.styled-active a').css({
-//                'color':'#F15A29'
-//            });
+            //            $('.styled-active a').css({
+            //                'color':'#F15A29'
+            //            });
 
         } else {
             $('.main-header').css({
@@ -32,7 +32,7 @@ $(document).ready(function () {
             });
 
             $('.styled-active a').css({
-                'color':'#F15A29'
+                'color': '#F15A29'
             });
         };
 
@@ -40,11 +40,25 @@ $(document).ready(function () {
 
 });
 
+$(".package-pp").mouseenter(function () {
+        $(".package-pp img").animate({
+            "opacity": "0.5"
+        }, 500, "swing");
+
+        $(".package-pp h4").fadeIn(500);
+    })
+    .mouseleave(function () {
+        $(".package-pp img").animate({
+            "opacity": "1"
+        }, 500, "swing");
+
+        $(".package-pp h4").fadeOut(500);
+    });
+
 // This fades the page in
-$(window).on('load', function () {
-
+$(window).on("load", function (e) {
+    e.preventDefault();
     $(document.body).hide().fadeIn(1000);
-
 });
 
 // Footer Date update
