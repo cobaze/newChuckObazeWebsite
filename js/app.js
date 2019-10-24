5759c9df13a7808e89f6aebe078a2a4975d21bdb
Chuck Obaze <chuckobaze@gmail.com>
2019-10-24 02:17:53 -0400

    Stopped here - 10-24-19

--- js/app.js
+++ js/app.js
@@ -46,9 +46,19 @@ $(document).ready(function () {
         showSlides(slideIndex = n);
     }
 
-    function showSlide(n) {
+    function showSlides(n) {
         var i;
         var slides = document.getElementsByClassName("mySlides");
+        if (n > slides.length) {
+            slideIndex = 1
+        }
+        if (n < 1) {
+            slideIndex = slides.length
+        }
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slides[slideIndex - 1].style.display = "block";
     }
 });
 
